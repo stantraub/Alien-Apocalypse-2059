5fb0e8bd4fa3fac6a42a01266dbec9c17c06496b
Charles Muldoon <charm3728@gmail.com>
2019-09-24 11:30:09 -0700

    added movement

---
+++ frontend/gamelogic/logic/index.js
@@ -0,0 +1,6 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const canvasEl = document.getElementsByTagName("canvas")[0];
+//   canvasEl.width = Game.DIM_X;
+//   canvasEl.height = Game.DIM_Y;
+
+// });
