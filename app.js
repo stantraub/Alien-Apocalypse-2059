5a0b97a36ac3efc04c090474809026d74e9a1e04
Stanley <stanley.traub@gmail.com>
2019-09-23 18:01:31 -0700

    Start User Auth

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
+const users = require("./routes/api/users");
+
 mongoose 
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB sucessfully"))
     .catch(err => console.log(err));
+
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 
