5b0e5b57be6dac410bf6c6f3ea1475d3c6dbd70a
Charles Muldoon <charm3728@gmail.com>
2019-09-25 09:53:04 -0700

    separated game logic into folders

--- frontend/gamelogic/logic/index.js
+++
@@ -1,6 +0,0 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const canvasEl = document.getElementsByTagName("canvas")[0];
-//   canvasEl.width = Game.DIM_X;
-//   canvasEl.height = Game.DIM_Y;
-
-// });
