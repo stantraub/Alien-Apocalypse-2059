dec4ed5691acba359b8ffa59be3c9ac18756fb49
Charles Muldoon <charm3728@gmail.com>
2019-10-09 17:00:37 -0700

    game works on login

--- frontend/src/components/game/game.jsx
+++ frontend/src/components/game/game.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import './game_container';
-
+import GameFile from "./game";
+import GameView from "./game_view";
 class Game extends React.Component {
     constructor(props){
         super(props)
     this.handleOnClick = this.handleOnClick.bind(this);    
     }
+    componentDidMount(){
+        console.log("working")
+        const canvasEl = document.getElementById("game-canvas");
 
+        canvasEl.width = GameFile.DIM_X;
+        canvasEl.height = GameFile.DIM_Y;
+
+        const ctx = canvasEl.getContext("2d");
+        const game = new GameFile();
+        window.game = game;
+        new GameView(game, ctx).start();
+    }
     handleOnClick(e){
         e.preventDefault();
         
@@ -14,26 +26,26 @@ class Game extends React.Component {
     }
 
 
-render(){
-    let game;
-    if(this.props.user){
-        game =
+    render(){
+        let game;
+        if(this.props.user){
+            game =
+            <div>
+                <h1>Success</h1>
+                <button onClick={this.handleOnClick}>Logout</button>
+                <canvas id="game-canvas" style={{height: 600, width: 1000, backgroundColor: 'gray',}}></canvas>
+                {/* <script type="application/javascript" src="../vendor/keymaster.js"></script> */}
+                {/* <script src="./index.js" type="application/javascript"></script> */}
+            
+            </div>
+        }
+        return (
         <div>
-            <h1>Success</h1>
-            <button onClick={this.handleOnClick}>Logout</button>
-            <canvas id="game-canvas" style={{height: 600, width: 1000, backgroundColor: 'gray',}}></canvas>
-            <script type="application/javascript" src="../vendor/keymaster.js"></script>
-            <script src="./main.js" type="application/javascript"></script>
-        
+            {game}
         </div>
-    }
-    return (
-       <div>
-           {game}
-       </div>
-    )
+        )
 
-}
+    }
 
 }
 
