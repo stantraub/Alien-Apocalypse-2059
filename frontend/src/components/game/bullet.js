f153fd3e2b7c15c28af852a9727d0b1f7de065df
LamVu1 <lvu2548@gmail.com>
2019-10-16 16:39:55 -0700

    Fixed Sprites

--- frontend/src/components/game/bullet.js
+++ frontend/src/components/game/bullet.js
@@ -16,8 +16,8 @@ class Bullet extends MovingObject {
     this.yVel = 0;
     this.game = options.game;
     this.mirror = options.mirror;
-    this.player = new Image();
-    this.player.src = "energy_effect_base.png";
+    this.Bullet = new Image();
+    this.Bullet.src = "energy_effect_base.png";
     
     
   }
@@ -35,13 +35,13 @@ class Bullet extends MovingObject {
     // ctx.rect(this.x, this.y, this.width, this.height);
     // ctx.fill();
     // let player = new Image();
-    // if (this.mirror) {
-    //   player.src = "energy_effect_base.png";
-      ctx.drawImage(this.player, 0, 100, 30, 25, this.x, this.y - 40, 50, 50);
-    // } else {
-    //   player.src = "reverse_bullet.png";
-    //   ctx.drawImage(player, 450, 100, 30, 25, this.x, this.y - 30, 50, 50);
-    // }
+    if (this.mirror) {
+      this.Bullet.src = "energy_effect_base.png";
+      ctx.drawImage(this.Bullet, 0, 100, 30, 25, this.x, this.y - 40, 50, 50);
+    } else {
+      this.Bullet.src = "reverse_bullet.png";
+      ctx.drawImage(this.Bullet, 450, 100, 30, 25, this.x, this.y - 30, 50, 50);
+    }
   
   };
   collideWith(otherObject) {
